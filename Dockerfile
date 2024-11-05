0391e1ad5928acfb9b5278636aa2326a95ca8873
Matheus Freitas <fernandes.matheus@aluno.uece.br>
2024-11-04 23:35:02 -0300

    Start server implementation

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM golang:1.23.2-bullseye AS builder
+
+WORKDIR /go/src/app
+COPY . .
+
+RUN go mod download
+RUN CGO_ENABLED=0 go build -o /go/bin/app
+
+FROM gcr.io/distroless/base-debian12
+COPY --from=builder /go/bin/app /
+CMD ["/app"]
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode("success")
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
