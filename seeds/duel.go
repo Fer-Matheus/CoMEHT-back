d88e0131c00f66350b9e5e7e7381bf7eb31a8105
Matheus Freitas <fernandes.matheus@aluno.uece.br>
2024-11-24 01:31:53 -0300

    Update duels seed

--- seeds/duel.go
+++ seeds/duel.go
@@ -6,16 +6,34 @@ import (
 )
 
 func SeedDuels(userId int) {
+	
+	var user models.User
+	_ = database.Db.GetUserById(userId, &user)
+
+	var duels []models.Duel
+
+	_ = database.Db.GetAllDuels(&duels)
+
+	if len(duels) > 0 {
+		for _, duel := range duels{
+			duel.Users = append(duel.Users, user)
+			database.Db.UpdateDuel(&duel)
+		}
+		return
+	}
+
+	users := []models.User{user}
+
 	seeds := []models.Duel{
 		{
 			CommitMessageAId: 1,
 			CommitMessageBId: 2,
-			UserId:           userId,
+			Users: users,
 		},
 		{
 			CommitMessageAId: 3,
 			CommitMessageBId: 4,
-			UserId:           userId,
+			Users: users,
 		},
 	}
 	_ = database.Db.SaveDuels(&seeds)
